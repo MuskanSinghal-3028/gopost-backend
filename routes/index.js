324a7ffcf212dbb95bfd1e13e22f3e7b766a99cf
Muskan <muskansinghalofficial@gmail.com>
2021-05-30 16:13:30 +0530

    updated

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,20 @@ const router = express.Router();
 
 
 router.use('/api', require('./api'));
+// router.get('/',function(req,res){
+
+// })
+// router.get('/',(req,res)=>{
+//     res.send(GetResponse);
+// })
+// const GetResponse = `<pre>How To Use This API
+//     1. Method of request must be POST
+//     2. URL is https://globalshalamail.herokuapp.com/
+//     3. Set the headers of request as follows:
+//         a. 'Accept':'application/json, text/plain, /'
+//         b. 'Content-Type':'application/json'
+//     4. Set the body of request, for syntax check the /input formate/sample.txt</pre>`;
+
 
 
 
