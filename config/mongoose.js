f9dccb8245ff1693666005d36665048f2ec1db0d
Muskan <muskansinghalofficial@gmail.com>
2021-05-30 16:55:17 +0530

    updated.

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://Muskan:liyXObl4jfLXQwG0@cluster0.9nmeo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+// mongoose.connect('mongodb+srv://Muskan:liyXObl4jfLXQwG0@cluster0.9nmeo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+
+
+
+
+// WV4JbInkqPehcaZw
+mongoose.connect("mongodb+srv://MuskanSinghal:WV4JbInkqPehcaZw@cluster0.9nmeo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
@@ -8,8 +17,9 @@ db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
 
 
 db.once('open', function(){
-    // console.log('Connected to Database :: MongoDB');
+     console.log('Connected to Database :: MongoDB');
 });
+// WV4JbInkqPehcaZw
 
 
 module.exports = db;
